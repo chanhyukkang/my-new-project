c8b37947abd24b4b1fa15f4e6dc374d0fbd96b45
rkdcksgur <rkdcksgur@ajou.ac.kr>
2019-07-26 18:17:57 +0900

    add weather description in WeatherDetailScreen

--- WeatherDetailScreen.js
+++ WeatherDetailScreen.js
@@ -44,13 +44,17 @@ render() {
      let celsius = this.state.main.temp - 273.15;
      let tempmin = this.state.main.temp_min -273.15;
      let tempmax = this.state.main.temp_max -273.15;
+     let weatherdescription = this.state.weather[0].description;
+     //let weatherIcon = this.state.weather.icon;
 
 
    return(
        <View style={styles.container}>
-
+        <Text style={styles.weather}>
+            날씨 : {weatherdescription}
+        </Text>
         <Text style={styles.temperature}>
-            현재기온 : {celsius.toFixed(1)}°C
+            {'\n'}현재기온 : {celsius.toFixed(1)}°C
         </Text>
         <Text style={styles.temperature}>
               최고 / 최저기온 : {tempmax.toFixed(1)}°C / {tempmin.toFixed(1)}°C
@@ -74,7 +78,12 @@ render() {
         textAlign: 'center',
         marginTop: 300,
       },
-      
+      weather: {
+          fontSize: 30,
+          color: 'white',
+         textAlign: 'center',
+         margin: 10,
+      },
       temperature: {
           fontSize: 25,
           color: 'white',
